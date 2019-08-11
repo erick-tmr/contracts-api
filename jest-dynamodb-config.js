97193c2dd5ba0066774c446f0eecd0e9845f7959
Erick Takeshi <erick.tmr@outlook.com>
2019-08-11 18:52:00 -0300

    jest dynamodb config

---
+++ jest-dynamodb-config.js
@@ -0,0 +1,105 @@
+'use strict'
+
+module.exports = {
+  tables: [
+    {
+      AttributeDefinitions: [
+        {
+          AttributeName: "pk", 
+          AttributeType: "S"
+        }, 
+        {
+          AttributeName: "sk", 
+          AttributeType: "S"
+        },
+        {
+          AttributeName: "data", 
+          AttributeType: "S"
+        },
+        {
+          AttributeName: "filter", 
+          AttributeType: "S"
+        },
+      ],
+      KeySchema: [
+        {
+          AttributeName: "pk", 
+          KeyType: "HASH"
+        }, 
+        {
+          AttributeName: "sk", 
+          KeyType: "RANGE"
+        }
+      ],
+      ProvisionedThroughput: {
+        ReadCapacityUnits: 1, 
+        WriteCapacityUnits: 1
+      },
+      TableName: 'test-tk_contracts_api',
+      GlobalSecondaryIndexes: [
+        {
+          IndexName: 'gsi_1',
+          KeySchema: [
+            {
+              AttributeName: 'sk',
+              KeyType: 'HASH'
+            },
+            {
+              AttributeName: 'data',
+              KeyType: 'RANGE'
+            }
+          ],
+          Projection: {
+            ProjectionType: 'ALL'
+          },
+          ProvisionedThroughput: {
+            ReadCapacityUnits: 1,
+            WriteCapacityUnits: 1
+          }
+        },
+        {
+          IndexName: 'gsi_2',
+          KeySchema: [
+            {
+              AttributeName: 'data',
+              KeyType: 'HASH'
+            },
+            {
+              AttributeName: 'filter',
+              KeyType: 'RANGE'
+            }
+          ],
+          Projection: {
+            ProjectionType: 'ALL'
+          },
+          ProvisionedThroughput: {
+            ReadCapacityUnits: 1,
+            WriteCapacityUnits: 1
+          }
+        },
+        {
+          IndexName: 'gsi_3',
+          KeySchema: [
+            {
+              AttributeName: 'sk',
+              KeyType: 'HASH'
+            },
+            {
+              AttributeName: 'filter',
+              KeyType: 'RANGE'
+            }
+          ],
+          Projection: {
+            ProjectionType: 'ALL'
+          },
+          ProvisionedThroughput: {
+            ReadCapacityUnits: 1,
+            WriteCapacityUnits: 1
+          }
+        }
+      ]
+    }
+    // etc
+  ],
+  port: 5000
+};
