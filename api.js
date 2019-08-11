5880d6fa1d94f43ed222364ffb0381e3cdb6d98e
Erick Takeshi <erick.tmr@outlook.com>
2019-08-11 18:51:38 -0300

    api routes

---
+++ api.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const Api = require('claudia-api-builder');
+const api = new Api({ mergeVars: true });
+
+api.get('/', () => 'Contracts API');
+
+const createUsersController = require('./src/controllers/create-users-controller');
+api.post('/users', createUsersController);
+
+api.get('/contracts', (request) => {
+  return [
+    {
+      id: '1',
+      name: 'Contrato 1',
+      value: '15000000'
+    },
+    {
+      id: '1',
+      name: 'Contrato 2',
+      value: '5000000'
+    }
+  ];
+});
+
+module.exports = api;
