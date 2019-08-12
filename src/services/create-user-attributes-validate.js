f21856c5f9f7beff682e7778dc3f3637a5730499
Erick Takeshi <erick.tmr@outlook.com>
2019-08-12 14:36:14 -0300

    user validator

---
+++ src/services/create-user-attributes-validate.js
@@ -0,0 +1,54 @@
+'use strict'
+
+const baseValidator = require('./base-validator');
+
+module.exports = (attributes) => {
+  const validatableFields = [
+    'firstName',
+    'lastName',
+    'email',
+    'cpf'
+  ];
+  const validator = (fieldName, value) => {
+    return {
+      firstName: () => {
+        const errors = [];
+  
+        if (!value) {
+          errors.push('firstName is required.');
+        }
+  
+        return errors;
+      },
+      lastName: () => {
+        const errors = [];
+  
+        if (!value) {
+          errors.push('lastName is required.');
+        }
+  
+        return errors;
+      },
+      email: () => {
+        const errors = [];
+  
+        if (!value) {
+          errors.push('email is required.');
+        }
+  
+        return errors;
+      },
+      cpf: () => {
+        const errors = [];
+  
+        if (!value) {
+          errors.push('cpf is required.');
+        }
+  
+        return errors;
+      }
+    }[fieldName]();
+  };
+
+  return baseValidator(attributes, validator, validatableFields);
+};
