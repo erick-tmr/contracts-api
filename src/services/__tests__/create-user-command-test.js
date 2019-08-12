7d9c6ca31b06a7473430d008f46d2453bc70251a
Erick Takeshi <erick.tmr@outlook.com>
2019-08-12 14:37:26 -0300

    create user command spec

---
+++ src/services/__tests__/create-user-command-test.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const createUserCommand = require('../create-user-command');
+
+const fakeRepository = {
+  create: (params) => Promise.resolve(params)
+};
+
+it('validates the user attributes', () => {
+  const userAttributes = {
+    firstName: 'Person',
+    lastName: 'Name',
+    cpf: '40706332633',
+    montlyIncome: 1000000, 
+  };
+
+  const { validations, success } = createUserCommand(userAttributes, fakeRepository);
+  expect(success).toBe(false);
+  expect(validations).toEqual(
+    expect.arrayContaining(['email is required.'])
+  );
+})
+
+describe('without errors', () => {
+  it('returns success true', async () => {
+    const userAttributes = {
+      firstName: 'Person',
+      lastName: 'Name',
+      cpf: '40706332633',
+      email: 'my@email.com',
+      montlyIncome: 1000000 
+    };
+
+    const { success } = await createUserCommand(userAttributes, fakeRepository);
+    expect(success).toBe(true);
+  })
+
+  it('returns the created user in resource key', async () => {
+    const userAttributes = {
+      firstName: 'Person',
+      lastName: 'Name',
+      cpf: '40706332633',
+      email: 'my@email.com',
+      montlyIncome: 1000000 
+    };
+
+    const { resource } = await createUserCommand(userAttributes, fakeRepository);
+    expect(resource).toMatchObject(userAttributes);
+  })
+})
