1e92ce88e45d3f0ee7b5257a7cdab58a7fe5192d
Erick Takeshi <erick.tmr@outlook.com>
2019-08-12 14:35:58 -0300

    use repository

---
+++ src/repositories/user-repository.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const baseRepository = require('./base-repository');
+const uuidv4 = require('uuid/v4');
+
+const tableName = 'tk_contracts_api';
+const baseRepositoryInstance = baseRepository(tableName);
+
+module.exports = {
+  create: (userModel) => {
+    const item = {
+      pk: uuidv4(),
+      sk: 'User',
+      data: userModel.email,
+      filter: `${userModel.firstName}#${userModel.lastName}`,
+      ...userModel
+    };
+    const { create } = baseRepositoryInstance;
+
+    return create({ item });
+  }
+};
