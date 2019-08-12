6f28e66476743d2d264f6952dd2bd3a0697609bc
Erick Takeshi <erick.tmr@outlook.com>
2019-08-12 14:36:05 -0300

    user repo spec

---
+++ src/repositories/__tests__/user-repository-test.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const userRepository = require('../user-repository');
+const userModel = require('../../models/user-model');
+
+jest.mock('../base-repository', () => () => ({
+  create: params => params
+}));
+
+it('returns an object with create method', () => {
+  expect(userRepository).toHaveProperty('create');
+})
+
+describe('.create', () => {
+  it('creates an user', () => {
+    const userAttributes = {
+      firstName: 'My',
+      lastName: 'User',
+      email: 'user@email.com'
+    }
+    const user = userModel(userAttributes);
+    
+    const response = userRepository.create(user);
+    expect(response.item).toMatchObject(userAttributes);
+  })
+
+  it('generates a partition key', () => {
+    const userAttributes = {
+      firstName: 'My',
+      lastName: 'User',
+      email: 'user@email.com'
+    }
+    const user = userModel(userAttributes);
+    
+    const response = userRepository.create(user);
+    expect(response.item.pk).toBeDefined();
+  })
+})
