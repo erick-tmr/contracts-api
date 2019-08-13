535d1721e7bd5101322ab22868ab789e4d667afd
Erick Takeshi <erick.tmr@outlook.com>
2019-08-13 18:27:09 -0300

    create contracts controller

---
+++ src/controllers/create-contracts-controller.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const creteContractCommand = require('../services/create-contract-command');
+const contractRepository = require('../repositories/contract-repository');
+
+module.exports = (request) => {
+  const { body, pathParams } = request;
+
+  const contractBody = {
+    amount: body.amount,
+    userId: pathParams.userId
+  };
+
+  return creteContractCommand(contractBody, contractRepository);
+};
