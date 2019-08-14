2e65194d20188d2cda4db30473b5710d4cbe0712
Erick Takeshi <erick.tmr@outlook.com>
2019-08-14 18:46:02 -0300

    add routes

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 const Api = require('claudia-api-builder');
 const api = new Api({ mergeVars: true });
@@ -11,19 +11,10 @@ api.post('/users', createUsersController);
 const createContractsController = require('./src/controllers/create-contracts-controller');
 api.post('/users/{userId}/contracts', createContractsController);
 
-api.get('/contracts', (request) => {
-  return [
-    {
-      id: '1',
-      name: 'Contrato 1',
-      value: '15000000'
-    },
-    {
-      id: '1',
-      name: 'Contrato 2',
-      value: '5000000'
-    }
-  ];
-});
+const updateUsersController = require('./src/controllers/update-users-controller');
+api.put('/users/{userId}', updateUsersController);
+
+const updateContractsController = require('./src/controllers/update-contracts-controller');
+api.put('/contracts/{contractId}', updateContractsController);
 
 module.exports = api;
