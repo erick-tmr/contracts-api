4c20afed47157ce8f4c1954c5fabd2d1a52e0a87
Erick Takeshi <erick.tmr@outlook.com>
2019-08-14 11:29:43 -0300

    contract repo crud

--- src/repositories/contract-repository.js
+++ src/repositories/contract-repository.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const baseRepository = require('./base-repository');
+const contractModelBuilder = require('../models/contract-model');
 const uuidv4 = require('uuid/v4');
 
 const tableName = 'tk_contracts_api';
@@ -17,6 +18,34 @@ module.exports = {
     };
     const { create } = baseRepositoryInstance;
 
-    return create({ item });
+    return create(item);
+  },
+  list: () => {
+    const conditionExpression = 'sk = :pk';
+    const expressionValues = {
+      ':pk': 'Contract'
+    };
+    const indexName = 'gsi_1';
+    const { query } = baseRepositoryInstance;
+
+    return query(conditionExpression, expressionValues, indexName)
+      .then(({ Items, Count }) => {
+        return {
+          items: Items.map(attributes => contractModelBuilder(attributes)),
+          count: Count
+        };
+      });
+  },
+  update: (contractModel) => {
+    const item = {
+      pk: contractModel.pk,
+      sk: 'Contract',
+      data: contractModel.userId,
+      filter: contractModel.status,
+      ...contractModel
+    };
+    const { update } = baseRepositoryInstance;
+
+    return update(item);
   }
 };
