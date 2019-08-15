50d8f130bc9fda6d441b3359be40af01f6a9664d
Erick Takeshi <erick.tmr@outlook.com>
2019-08-15 09:08:23 -0300

    routes

--- api.js
+++ api.js
@@ -11,10 +11,10 @@ api.post('/users', createUsersController);
 const createContractsController = require('./src/controllers/create-contracts-controller');
 api.post('/users/{userId}/contracts', createContractsController);
 
-const updateUsersController = require('./src/controllers/update-users-controller');
-api.put('/users/{userId}', updateUsersController);
+// const updateUsersController = require('./src/controllers/update-users-controller');
+// api.put('/users/{userId}', updateUsersController);
 
-const updateContractsController = require('./src/controllers/update-contracts-controller');
-api.put('/contracts/{contractId}', updateContractsController);
+// const updateContractsController = require('./src/controllers/update-contracts-controller');
+// api.put('/contracts/{contractId}', updateContractsController);
 
 module.exports = api;
